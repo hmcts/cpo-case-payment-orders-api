1993b2e94a40f7c27a9b388cf9788b6ec95c2c0a
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-03-16 18:30:41 +0000

    key-vault and app-insights

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,17 +1,25 @@
-variable "product" {}
+// Infrastructural variables
+variable "product" {
+}
 
-variable "component" {}
+variable "component" {
+}
+
+variable "env" {
+}
 
 variable "location" {
   default = "UK South"
 }
 
-variable "env" {}
-
-variable "subscription" {}
-
-variable "deployment_namespace" {}
-
 variable "common_tags" {
-  type = "map"
+  type = map(string)
+}
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
