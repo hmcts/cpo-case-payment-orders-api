312ecc8eb2fa7fedfe6655aa86404f7f8a7bfd7a
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-03-17 14:34:37 +0000

    Revert "Temporarily - Revert "CPO-12: Set up skeleton classes and initial entities and domain classes"" (#8)

---
+++ src/main/java/uk/gov/hmcts/reform/cpo/controllers/CasePaymentOrdersController.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.reform.cpo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CasePaymentOrdersController {
+}
---
+++ src/main/java/uk/gov/hmcts/reform/cpo/service/impl/CasePaymentOrdersServiceImpl.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.cpo.service.impl;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.cpo.service.CasePaymentOrdersService;
+
+@Service
+public class CasePaymentOrdersServiceImpl implements CasePaymentOrdersService {
+
+}
