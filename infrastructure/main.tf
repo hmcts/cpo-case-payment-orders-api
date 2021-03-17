9cb738f52d169cebadf2056ac0c59acb94175f43
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-03-17 13:51:35 +0000

    CPO-3 - terraform scripts to add database

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  app_full_name = "${var.product}-${var.component}"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
@@ -34,4 +38,59 @@ resource "azurerm_application_insights" "appinsights" {
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
   tags                = var.common_tags
+}
+
+module "cpo-case-payments-db" {
+  source                = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product               = var.product
+  component             = var.component
+  name                  = "${local.app_full_name}-postgres-db"
+  location              = var.location
+  env                   = var.env
+  subscription          = var.subscription
+  postgresql_user       = var.postgresql_user
+  postgresql_version    = var.postgresql_version
+  database_name         = var.database_name
+  sku_name              = var.sku_name
+  sku_tier              = var.sku_tier
+  sku_capacity          = var.sku_capacity
+  ssl_enforcement       = var.ssl_enforcement
+  storage_mb            = var.storage_mb
+  backup_retention_days = var.backup_retention_days
+  georedundant_backup   = var.georedundant_backup
+  common_tags           = var.common_tags
+}
+
+////////////////////////////////
+// Populate Vault with DB info
+////////////////////////////////
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.cpo-case-payments-db.user_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.cpo-case-payments-db.postgresql_password
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.cpo-case-payments-db.host_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = module.cpo-case-payments-db.postgresql_listen_port
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.cpo-case-payments-db.postgresql_database
+  key_vault_id = module.key-vault.key_vault_id
 }
