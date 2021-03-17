dd7915d03fd6e6adef38927a1375815232a3a005
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-03-17 16:33:09 +0000

    Swagger publisher

--- src/main/java/uk/gov/hmcts/reform/cpo/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/config/SwaggerConfiguration.java
@@ -2,15 +2,21 @@ package uk.gov.hmcts.reform.cpo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.cpo.Application;
+
+import java.util.Arrays;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
@@ -18,8 +24,43 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
-            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
             .paths(PathSelectors.any())
+            .build().useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
+            .globalRequestParameters(Arrays.asList(
+                headerServiceAuthorization(),
+                headerAuthorization()
+            ));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Mange case assignment API")
+            .description("Mange case access")
+            .version("1.0.0")
+            .contact(new Contact("CDM",
+                                 "https://tools.hmcts.net/confluence/display/RCCD/Reform%3A+Core+Case+Data+Home",
+                                 "corecasedatateam@hmcts.net"))
+            .termsOfServiceUrl("")
+            .build();
+    }
+
+    private RequestParameter headerServiceAuthorization() {
+        return new RequestParameterBuilder()
+            .name("ServiceAuthorization")
+            .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
+            .in(ParameterType.HEADER)
+            .required(true)
+            .build();
+    }
+
+    private RequestParameter headerAuthorization() {
+        return new RequestParameterBuilder()
+            .name("Authorization")
+            .description("Keyword `Bearer` followed by a valid IDAM user token")
+            .in(ParameterType.HEADER)
+            .required(true)
             .build();
     }
 
