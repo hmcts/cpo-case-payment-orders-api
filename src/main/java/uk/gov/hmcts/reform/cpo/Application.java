3968c88456c1c9d725f16f6e2f9c22abe20ec641
Dan Lysiak <daniel.lysiak1@hmcts.net>
2021-03-18 17:48:46 +0000

    Add in testcontainers for postgres

--- src/main/java/uk/gov/hmcts/reform/cpo/Application.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/Application.java
@@ -2,18 +2,10 @@ package uk.gov.hmcts.reform.cpo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
-// FIXME: once CPO-3 is merged, remove the exclude options
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class,
-    DataSourceTransactionManagerAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableFeignClients(basePackageClasses = IdamApi.class)
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
