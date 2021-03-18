a14561184fe86b2c4cfbb014685c95b2b6a2317b
Tom Elliott <tomxelliott@hotmail.co.uk>
2021-03-18 17:35:36 +0000

    CPO-15: Add temp fix for SpringBootApplication start up - to be removed once CPO-3 is merged

--- src/main/java/uk/gov/hmcts/reform/cpo/Application.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/Application.java
@@ -2,10 +2,18 @@ package uk.gov.hmcts.reform.cpo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
-@SpringBootApplication
+// FIXME: once CPO-3 is merged, remove the exclude options
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 @EnableFeignClients(basePackageClasses = IdamApi.class)
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
