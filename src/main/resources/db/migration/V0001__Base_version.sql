3e25e3f8ae1815bd26501e45e859ae3ae1a8bac5
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-03-19 07:30:40 +0000

    CPO-3 - updates following code review

--- src/main/resources/db/migration/V0001__Base_version.sql
+++ src/main/resources/db/migration/V0001__Base_version.sql
@@ -1,13 +1,13 @@
 CREATE TABLE public.case_payment_orders (
-                         id integer not null PRIMARY KEY,
+                         id uuid not null PRIMARY KEY,
                          created_timestamp timestamp without time zone not null,
                          effective_from timestamp without time zone not null,
                          case_id BIGINT not null,
-                         case_type_id character varying(70),
+                         case_type_id character varying(70) not null,
                          action character varying(70) not null,
                          responsible_party character varying(1024) not null,
                          order_reference character varying(70) not null,
-                         created_by character varying(70)
+                         created_by character varying(70) not null
 );
 
 ALTER TABLE ONLY public.case_payment_orders
