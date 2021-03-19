0b1db265ef0faaddee13349bba3d80a5b083b917
it.consulting@tutanota.com <it.consulting@tutanota.com>
2021-03-19 14:44:35 +0000

    CPO-7: Build GetPaymentOrders endpoint        [CPO-7] (https://tools.hmcts.net/jira/browse/CPO-7).

--- src/main/java/uk/gov/hmcts/reform/cpo/controllers/CasePaymentOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/controllers/CasePaymentOrdersController.java
@@ -1,7 +1,42 @@
 package uk.gov.hmcts.reform.cpo.controllers;
 
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.cpo.domain.CasePaymentOrder;
+import uk.gov.hmcts.reform.cpo.service.impl.CasePaymentOrdersServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/api")
 public class CasePaymentOrdersController {
+
+    private final CasePaymentOrdersServiceImpl casePaymentOrdersServiceImpl;
+
+    public CasePaymentOrdersController(CasePaymentOrdersServiceImpl casePaymentOrdersServiceImpl) {
+        this.casePaymentOrdersServiceImpl = casePaymentOrdersServiceImpl;
+    }
+
+
+    @GetMapping(value = "cases/payments/orders", produces = {"application/json"})
+    public List<CasePaymentOrder> getCasePaymentOrders(@ApiParam(value = "list of ids")
+                                                       @RequestParam("ids") Optional<List<String>> ids,
+                                                       @ApiParam(value = "list of ids")
+                                                       @RequestParam("ids") Optional<List<String>> casesId,
+                                                       @RequestParam("ids") Optional<Integer> pageSize,
+                                                       @RequestParam("ids") Optional<Integer> pageNumber
+
+    ){
+        return casePaymentOrdersServiceImpl.getCasePaymentOrders(
+            ids,
+            casesId,
+            pageSize.orElse(CasePaymentOrdersServiceImpl.DEFAULT_PAGE_SIZE),
+            pageNumber.orElse(CasePaymentOrdersServiceImpl.DEFAULT_PAGE_NUMBER)
+        );
+    }
+
 }
--- src/main/java/uk/gov/hmcts/reform/cpo/repository/CasePaymentOrdersRepository.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/repository/CasePaymentOrdersRepository.java
@@ -1,10 +1,23 @@
 package uk.gov.hmcts.reform.cpo.repository;
 
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.cpo.data.CasePaymentOrderEntity;
 
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
 @Repository
 public interface CasePaymentOrdersRepository extends JpaRepository<CasePaymentOrderEntity, Long> {
+
+    @Query("select b from CasePaymentOrdersRepository b where b.id in :ids")
+    Slice<CasePaymentOrderEntity> getCasePaymentOrdersByIds(@Param("ids") List<String> ids, Pageable pageable);
+
+
+    @Query("select b from CasePaymentOrdersRepository b where b.caseId in :casesId")
+    Slice<CasePaymentOrderEntity> getCasePaymentOrdersByCaseIds(@Param("casesId") List<String> casesId, Pageable pageable);
 
 }
