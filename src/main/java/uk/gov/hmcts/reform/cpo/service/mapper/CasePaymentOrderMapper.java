d20cc341780a0a327285c76c05d47369a417a139
Abi-Laver <abi.laver@cgi.com>
2021-03-19 17:53:44 +0000

    initial commit

--- src/main/java/uk/gov/hmcts/reform/cpo/service/mapper/CasePaymentOrderMapper.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/service/mapper/CasePaymentOrderMapper.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.cpo.service.mapper;
 
 import org.mapstruct.Mapper;
+import uk.gov.hmcts.reform.cpo.payload.CasePaymentOrderRequest;
 import uk.gov.hmcts.reform.cpo.data.CasePaymentOrderEntity;
 import uk.gov.hmcts.reform.cpo.domain.CasePaymentOrder;
+
+import java.time.LocalDateTime;
 
 @Mapper(componentModel = "spring")
 public interface CasePaymentOrderMapper {
@@ -10,4 +13,7 @@ public interface CasePaymentOrderMapper {
     CasePaymentOrderEntity toEntity(CasePaymentOrder casePaymentOrder);
 
     CasePaymentOrder toDomainModel(CasePaymentOrderEntity casePaymentOrderEntity);
+
+    CasePaymentOrder toRequest(CasePaymentOrderRequest casePaymentOrderRequest, String createdBy,
+                               LocalDateTime createdTimestamp);
 }
