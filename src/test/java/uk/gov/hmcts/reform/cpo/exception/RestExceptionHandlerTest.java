5ed32283b625330afa5b4a69a444ef4c226d6fc6
it.consulting@tutanota.com <it.consulting@tutanota.com>
2021-04-08 15:10:49 +0100

    CPO-7: Build GetPaymentOrders endpoint        [CPO-7] (https://tools.hmcts.net/jira/browse/CPO-7).

---
+++ src/test/java/uk/gov/hmcts/reform/cpo/exception/RestExceptionHandlerTest.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.cpo.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import uk.gov.hmcts.reform.BaseTest;
+import uk.gov.hmcts.reform.TestIdamConfiguration;
+import uk.gov.hmcts.reform.cpo.ApplicationParams;
+import uk.gov.hmcts.reform.cpo.config.SecurityConfiguration;
+import uk.gov.hmcts.reform.cpo.controllers.CasePaymentOrdersController;
+
+import uk.gov.hmcts.reform.cpo.security.JwtGrantedAuthoritiesConverter;
+import uk.gov.hmcts.reform.cpo.service.impl.CasePaymentOrdersServiceImpl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@WebMvcTest(controllers = CasePaymentOrdersController.class,
+    includeFilters = @ComponentScan.Filter(type = ASSIGNABLE_TYPE, classes = MapperConfig.class),
+    excludeFilters = @ComponentScan.Filter(type = ASSIGNABLE_TYPE, classes =
+        {SecurityConfiguration.class, JwtGrantedAuthoritiesConverter.class}))
+@AutoConfigureMockMvc(addFilters = false)
+@ImportAutoConfiguration(TestIdamConfiguration.class)
+class RestExceptionHandlerTest implements BaseTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    protected CasePaymentOrdersServiceImpl service;
+
+    @MockBean
+    protected ApplicationParams applicationParams;
+
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    private void assertHttpErrorResponse(ResultActions result,
+                                         HttpStatus expectedStatus,
+                                         String expectedMessage) throws Exception {
+
+        result
+            .andExpect(status().is(expectedStatus.value()))
+            .andExpect(jsonPath("$.status").value(expectedStatus.value()))
+            .andExpect(jsonPath("$.error").value(expectedStatus.getReasonPhrase()))
+            .andExpect(jsonPath("$.message").value(expectedMessage));
+    }
+
+}
