0c2f66bf99eb4eadd3d48dedcf2e191e8da73eb6
Abi-Laver <abi.laver@cgi.com>
2021-04-12 16:10:30 +0100

    Match error message capitalization

--- src/main/java/uk/gov/hmcts/reform/cpo/validators/ValidationError.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/validators/ValidationError.java
@@ -14,7 +14,7 @@ public final class ValidationError {
     public static final String ID_REQUIRED = "ID is required.";
     public static final String ORDER_REFERENCE_REQUIRED = "Order Reference is required.";
     public static final String RESPONSIBLE_PARTY_REQUIRED = "Responsible Party is required.";
-    public static final String ORDER_REFERENCE_INVALID = "Order reference has invalid format.";
+    public static final String ORDER_REFERENCE_INVALID = "Order Reference has invalid format.";
 
 
     // additional constraints etc..
