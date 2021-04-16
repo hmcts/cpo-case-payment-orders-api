7a681ac4cc34b2a491b1b67fa91a006c90bc5438
ashleynoronha <ashleydeannoronha@gmail.com>
2021-04-16 16:46:16 +0100

    address review comments

--- src/functionalTest/java/uk/gov/hmcts/reform/cpo/befta/CaseIdGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cpo/befta/CaseIdGenerator.java
@@ -10,42 +10,44 @@ public class CaseIdGenerator {
         new CaseIdGenerator().generateValidCaseReference();
     }
 
-    public String generateValidCaseReference(){
+    public String generateValidCaseReference() {
         String timestamp = getCurrentTime();
         String randomFour = genRandomFourNumbers();
         int checkedDigit = getCheckDigit(timestamp,randomFour);
-        String UID = timestamp + randomFour + checkedDigit;
-        System.out.println(UID);
-        return UID;
+        String uid = timestamp + randomFour + checkedDigit;
+        System.out.println(uid);
+        return uid;
     }
 
-    public String generateInvalidCaseReference(){
+    public String generateInvalidCaseReference() {
         return RandomStringUtils.randomNumeric(16);
     }
 
     /**
-     * This is the timestamp to 10th of the second representing first 10 digits of the UID for a case
+     * This is the timestamp to 10th of the second representing first 10 digits of the UID for a case.
      * @return
      */
-    private String getCurrentTime(){
+    private String getCurrentTime() {
         long date = new Date().getTime();
         return String.valueOf(date).substring(0,11);
     }
 
-    private String genRandomFourNumbers(){
+    private String genRandomFourNumbers() {
         return RandomStringUtils.randomNumeric(4);
     }
 
-    private int getCheckDigit(String timestamp, String randomFour){
+    private int getCheckDigit(String timestamp, String randomFour) {
         String numberString = timestamp + randomFour;
         return checkSum(numberString, true);
     }
 
     private int checkSum(String numberString, boolean noCheckDigit) {
-        int sum = 0, checkDigit = 0;
+        int sum = 0;
+        int checkDigit = 0;
 
-        if (!noCheckDigit)
+        if (!noCheckDigit) {
             numberString = numberString.substring(0, numberString.length() - 1);
+        }
 
         boolean isDouble = true;
         for (int i = numberString.length() - 1; i >= 0; i--) {
@@ -54,15 +56,17 @@ public class CaseIdGenerator {
             isDouble = !isDouble;
         }
 
-        if ((sum % 10) > 0)
+        if ((sum % 10) > 0) {
             checkDigit = (10 - (sum % 10));
+        }
 
         return checkDigit;
     }
 
     private int sumToSingleDigit(int k) {
-        if (k < 10)
+        if (k < 10) {
             return k;
+        }
         return sumToSingleDigit(k / 10) + (k % 10);
     }
 }
--- src/functionalTest/java/uk/gov/hmcts/reform/cpo/befta/CasePaymentOrdersTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cpo/befta/CasePaymentOrdersTestAutomationAdapter.java
@@ -3,8 +3,14 @@ package uk.gov.hmcts.reform.cpo.befta;
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultBeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
+import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CasePaymentOrdersTestAutomationAdapter extends DefaultTestAutomationAdapter {
+
+    private static Map<String, String> uniqueStringsPerTestData = new ConcurrentHashMap<>();
 
     @Override
     public BeftaTestDataLoader getDataLoader() {
@@ -15,4 +21,30 @@ public class CasePaymentOrdersTestAutomationAdapter extends DefaultTestAutomatio
             }
         };
     }
+
+    public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
+        if (key.toString().equals("GenerateCaseId")) {
+
+            String scenarioTag;
+            try {
+                scenarioTag = scenarioContext.getParentContext().getCurrentScenarioTag();
+            } catch (NullPointerException e) {
+                scenarioTag = scenarioContext.getCurrentScenarioTag();
+            }
+
+            String caseId = new CaseIdGenerator().generateValidCaseReference();
+            uniqueStringsPerTestData.put(scenarioTag,caseId);
+            return caseId;
+        } else if (key.toString().equals("GetGeneratedCaseId")) {
+            String scenarioTag;
+            try {
+                scenarioTag = scenarioContext.getParentContext().getCurrentScenarioTag();
+            } catch (NullPointerException e) {
+                scenarioTag = scenarioContext.getCurrentScenarioTag();
+            }
+            return Long.parseLong(uniqueStringsPerTestData.get(scenarioTag));
+        }
+        return super.calculateCustomValue(scenarioContext, key);
+    }
+
 }
