8c9f5ec00da0c4ac832fea963ef71ffeaa734e11
Matt Nayler <matt.nayler@cgi.com>
2021-05-12 09:27:09 +0100

    Hide root controller from swagger
    * Hide `RootController` from swagger as controller is only there to
      support azures "Always On" checks.
    * Remove `DemoUnitTest` class.

--- src/main/java/uk/gov/hmcts/reform/cpo/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/controllers/RootController.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.cpo.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@ApiIgnore
 @RestController
 public class RootController {
 
