0af3c4b73ef4ac162ac097170e319ade979d8b12
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2024-05-31 11:06:47 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -15,7 +15,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.103.1"
+      version = "~> 3.74"
     }
     azuread = {
       source  = "hashicorp/azuread"
