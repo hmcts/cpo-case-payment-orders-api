5f9367bd544bd058082fa71a804b5fdecf61bc34
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2024-05-31 11:20:06 +0100

    Update state.tf (#285)
    * Update state.tf
    
    * Update state.tf
    
    ---------
    
    Co-authored-by: dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -15,7 +15,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.74"
     }
     azuread = {
       source  = "hashicorp/azuread"
