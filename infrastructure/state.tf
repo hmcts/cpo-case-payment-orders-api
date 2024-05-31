4bcab2d0a8b641aacf7b3662dfc2fd008eadb339
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2024-05-31 10:44:35 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -15,7 +15,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.103.1"
     }
     azuread = {
       source  = "hashicorp/azuread"
