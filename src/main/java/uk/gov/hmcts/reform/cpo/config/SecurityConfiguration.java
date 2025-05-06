d9d670c847aaf0ef555de1a705da1dcc5fe27964
shahirali <shahirali@outlook.com>
2025-05-06 09:14:54 +0100

    CCD-5940: testing swagger-ui lists

--- src/main/java/uk/gov/hmcts/reform/cpo/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cpo/config/SecurityConfiguration.java
@@ -37,6 +37,7 @@ public class SecurityConfiguration {
     private final JwtAuthenticationConverter jwtAuthenticationConverter;
 
     private static final String[] AUTH_ALLOWED_LIST = {
+        "/**/webjars/**",
         "/**/v3/api-docs/**", //necessary
         "/swagger-resources/**",
         "/swagger-ui/**",
