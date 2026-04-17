73c50e1ad004130ff8c490cabdc0a9ef1e07a074
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-04-17 15:51:49 +0100

    add data resource and new attribute

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,12 +10,18 @@ resource "azurerm_resource_group" "rg" {
   tags = var.common_tags
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product                 = var.product
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
+  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_name      = "dcd_ccd"
   common_tags             = var.common_tags
