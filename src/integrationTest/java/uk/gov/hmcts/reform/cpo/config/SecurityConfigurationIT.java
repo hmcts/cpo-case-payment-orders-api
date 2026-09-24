8752c09ad31993cce6e810e88c55d4a3ea1b4167
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2026-09-24 09:50:02 +0100

    Remove unused field in test

--- src/integrationTest/java/uk/gov/hmcts/reform/cpo/config/SecurityConfigurationIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cpo/config/SecurityConfigurationIT.java
@@ -28,7 +28,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class SecurityConfigurationIT {
 
     private static final String ISSUER = "http://localhost:%s/o";
-    private static RSAKey rsaJwk;
 
     @Autowired
     private JwtDecoder jwtDecoder;
